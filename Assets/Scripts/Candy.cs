54c795a14d5dbf86319cd7dc5468c0095a9d4448
Atherz97 <athy97@gmail.com>
2016-11-01 16:53:13 -0400

    Add files via upload
    candy fixed

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -12,24 +12,27 @@ public class Candy : MonoBehaviour {
 	void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
         sound = GetComponent<AudioSource>();
+
+		//if (rigidBody.velocity.y < 0)
+		{
+			rigidBody.AddForce(new Vector2(150 * GameObject.FindGameObjectWithTag("Player").transform.localScale.x, 300));
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
     void FixedUpdate()
     {
-        if (rigidBody.velocity.y < 0f)
-        {
-            rigidBody.AddForce(new Vector2(launchSpeed, 0f));
-        }
+		
+		rigidBody.AddForce (new Vector2 (0,-5));
 
-        if (rigidBody.velocity.y >= 0f)
-        {
-            Invoke("deSpawn", 1.2f);
-        }
+		if (rigidBody.velocity.y == 0)
+		{
+			Invoke("deSpawn", 1.2f);
+		}
     }
 
     void OnCollisionEnter2D(Collision2D coll)
